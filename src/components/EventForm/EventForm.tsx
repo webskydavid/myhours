32f985fd7c67a3c7d1be47873fd5bf847fb90f8c
webskydavid <webskydavid@gmail.com>
2021-04-23 18:08:58 +0200

    fix: Command validation

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -1,5 +1,13 @@
 import { FC } from 'react';
-import { format, getDate } from 'date-fns';
+import {
+  differenceInHours,
+  format,
+  formatDistance,
+  getDate,
+  isAfter,
+  isBefore,
+  isValid,
+} from 'date-fns';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { useAtom } from 'jotai';
 import { currentDateAtom } from '../../atoms/app';
@@ -42,8 +50,8 @@ const EventForm: FC = () => {
         }}
         enableReinitialize={true}
         onSubmit={(values, { resetForm }) => {
-          resetForm();
-          return handleInsert(values);
+          //resetForm();
+          //return handleInsert(values);
         }}
       >
         <Form>
@@ -60,17 +68,37 @@ const EventForm: FC = () => {
                   0
                 ).getDate();
 
-                const regex = new RegExp(/^[0-9]{1,2} [0-9]{2,4} [0-9]{2,4}$/g);
+                const regex = new RegExp(/^[0-9]{1,2} [0-9]{3,4} [0-9]{3,4}$/g);
 
                 if (!regex.test(command)) {
                   error =
                     'Invalid value provided "14 1300 1400" <day starthour endhour>!';
                 } else {
-                  const [day] = command.split(' ');
+                  const y = format(currentDate, 'yyyy');
+                  const m = format(currentDate, 'MM');
+                  const [day, s, e] = command.split(' ');
+                  const sH = `${s.length < 4 ? '0' : ''}${s.slice(
+                    -4,
+                    s.length / 2
+                  )}:${s.slice(-2)}:00Z`;
+                  const eH = `${e.length < 4 ? '0' : ''}${e.slice(
+                    -4,
+                    e.length / 2
+                  )}:${e.slice(-2)}:00Z`;
+                  const sDate = `${y}-${m}-${day}T${sH}`;
+                  const eDate = `${y}-${m}-${day}T${eH}`;
                   const dayNumber = Number.parseInt(day);
 
+                  if (!isValid(new Date(sDate)) || !isValid(new Date(eDate))) {
+                    error = 'Invalid date format.';
+                  }
+
                   if (dayNumber > maxDay || dayNumber < 1) {
-                    error = 'Invalid day value!';
+                    error = 'Invalid day value.';
+                  }
+                  if (isAfter(new Date(sDate), new Date(eDate))) {
+                    error =
+                      'The starting hour cannot be less than the ending hour.';
                   }
                 }
                 return error;
